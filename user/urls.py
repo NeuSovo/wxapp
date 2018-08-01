582e402734011f7d81a66a46da879d8ce3cf75ae
zzde <zhangxh1997@gmail.com>
2018-08-01 15:15:03 +0800

    Merge pull request #7 from zxh326/daily
    * daily

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import  *
 
 urlpatterns = [
+    path('reg', RegUserView.as_view()),
     path('login', LoginUserView.as_view()),
     path('order', UserOrderView.as_view()),
     path('pt', PinTuanOrderView.as_view())
