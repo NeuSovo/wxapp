4ce5cbdb1c434390705cb56f123815e696582fc4
zzde <zhangxh1997@gmail.com>
2018-08-06 21:05:40 +0800

    apply done

--- course/views.py
+++ course/views.py
@@ -4,6 +4,7 @@ from dss.Mixin import (FormJsonResponseMixin, JsonResponseMixin,
                        MultipleJsonResponseMixin)
 from dss.Serializer import serializer
 from .models import *
+from user.auth import CheckUserWrap
 
 # Create your views here.
 class CourseListView(MultipleJsonResponseMixin, ListView):
@@ -58,6 +59,26 @@ class CourseChapterView(JsonResponseMixin, DetailView):
             [{'chapter': i, 'videos': serializer(i.coursechaptervideo_set.all(), exclude_attr=('chapter', ))} for i in self.object.coursechapter_set.all()],
             exclude_attr=('course', ))
         return context
+
+
+class ApplyCourseView(JsonResponseMixin, View, CheckUserWrap):
+    model = CourseApply
+
+    def post(self, request, *args, **kwargs):
+        if not self.wrap_check_token_result():
+            return self.render_to_response({'msg': self.msg})
+
+        course_id = kwargs.get('course_id')
+        try:
+            course = Course.objects.get(id=course_id)
+        except:
+            raise Http404()
+
+        if self.model.objects.filter(apply_user=self.user, apply_course=course).exists():
+            return self.render_to_response({'msg': '已报名'})
+
+        self.model.objects.create(apply_user=self.user, apply_course=course)
+        return self.render_to_response({'msg': 'ok'})
 
 
 def all_category_view(request):
