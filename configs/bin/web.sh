ffe917b87f884656e57f9f42daab5351788682c0
zzde <zhangxh1997@gmail.com>
2018-09-27 20:10:49 +0800

    update

--- configs/bin/web.sh
+++ configs/bin/web.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 pwd
 sleep 5
-python3 manage.py makemigrations user course goods order
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
+chown -R 1000 /wxapp
+su -m wxuser -c "python3 manage.py makemigrations user course goods order"
+su -m wxuser -c "python3 manage.py migrate"
+su -m wxuser -c "python3 manage.py collectstatic --no-input"
 chown -R 1000 media/
 su -m wxuser -c "uwsgi uwsgi.ini"
